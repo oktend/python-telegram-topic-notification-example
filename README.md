2edc8a178043c4552564813ed765d82d561f1af0
Vadim <rezvovvad@gmail.com>
2023-10-26 16:00:27 +0200

    robost version

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # python-telegram-topic-notification-example
+
+see main.py line 28, there you can set the log level
+see main.py line 41-45, only alerts with choosen level (33 line) and higher will be sent to telegram chat
+
+to run main.py you should:
+
+open console pressing ctrl + shift + ~ 
+
+# then run commands
+$ poetry install
+$ poetry shell 
+
+# then export credentials
+$ export TELEGRAM_NOTIFICATION_BOT_TOKEN=<bot_token>
+$ export TELEGRAM_NOTIFICATION_CHAT_ID=<chat_id>
+$ export REPLY_TO_MESSAGE_ID=<message_id>
+
+
+# then run main.py
+$ python main.py
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import logging
+from telegram_handler.handlers import TelegramHandler
+from telegram_handler.formatters import HtmlFormatter
+
+"""
+Configure logger: 
+https://docs.python.org/3/howto/logging.html#configuring-logging
+"""
+
+
+log = logging.getLogger('main')
+
+def init_logger(telegram_chat_token, telegram_chat_id, telegram_reply_to_message):
+
+    log.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+
+    stream_handler.setFormatter(formatter)
+
+    log.addHandler(stream_handler)
+
+    telegram_handler = TelegramHandler(level=logging.WARNING, token=telegram_chat_token, reply_to_message_id=telegram_reply_to_message, chat_id=telegram_chat_id)
+    telegram_handler.setLevel(logging.WARNING) # logging.WARNING sets the log level you will recieve in telegram chat to level WARNING and higher
+
+    formatter = HtmlFormatter()
+    telegram_handler.setFormatter(formatter)
+
+    log.addHandler(telegram_handler)
+
+def main():
+    init_logger(
+        os.getenv("TELEGRAM_NOTIFICATION_BOT_TOKEN"),
+        os.getenv("TELEGRAM_NOTIFICATION_CHAT_ID"),
+        os.getenv("REPLY_TO_MESSAGE_ID"),
+        )
+    log.info(f"this message level: info") # When you run the script, only alerts with choosen level (line 33) and higher will be sent to telegram chat
+    log.debug(f"this message level: debug")
+    log.warning(f"this message level: warning")
+    log.error(f"this message level: error")
+    log.critical(f"this message level: critical")
+
+if __name__ == "__main__":
+    main()
